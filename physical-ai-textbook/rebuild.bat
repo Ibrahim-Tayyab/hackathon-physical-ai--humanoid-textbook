485d45f529c5949a56fb77ceecf2eb75828e6057
Ibrahim-Tayyab <githubandvercel989@gmail.com>
2025-12-17 21:56:23 +0500

    ll

--- physical-ai-textbook/rebuild.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Clearing Docusaurus cache...
-if exist .docusaurus rmdir /s /q .docusaurus
-if exist build rmdir /s /q build
-if exist node_modules\.cache rmdir /s /q node_modules\.cache
-
-echo Starting development server...
-npm start
